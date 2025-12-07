1b0cd61ecdcdb17caadb0a326aa309180c099f14
Buddy <203337004+iambuddy01@users.noreply.github.com>
2025-12-08 02:08:06 +0530

    ᴅᴇᴠɴʙᴏᴛs

--- X/helpers/misc.py
+++ X/helpers/misc.py
@@ -141,10 +141,10 @@ async def create_botlog(client):
     LOGGER("X").info(
         "WAIT A MOMENT MASTER. CURRENTLY CREATING A USERBOT LOG GROUP FOR YOU"
     )
-    desc = "Group Log for Japanese-X-Userbot.\n\PLEASE DO NOT LEAVE THE GROUP INI.\n\nPowered By ~ @Japanese_Userbot ❤️"
+    desc = "Group Log for Userbot.\n\PLEASE DO NOT LEAVE THE GROUP INI.\n\nPowered By ~ #deleted_account ❤️"
     try:
         photo = "X/resources/logo.jpg"
-        gruplog = await client.create_supergroup("Japanese-X-Userbot Logs", desc)
+        gruplog = await client.create_supergroup("Userbot Logs", desc)
         fine = gruplog.id
         await client.set_chat_photo(fine, photo=photo)
         if await in_heroku():
